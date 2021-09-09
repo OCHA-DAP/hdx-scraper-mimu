cbda7a36f7e11bbb9e3abf55c32a2e3198ade794
Mike <rans@un.org>
2021-09-09 15:50:35 +1200

    Update to latest library

--- run.py
+++ run.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 """
 Top level script. Calls other functions that generate datasets that this script then creates in HDX.
 
 """
 import logging
 import re
-from os.path import join, expanduser
+from os.path import expanduser, join
 
+from hdx.facades.simple import facade
 from hdx.hdx_configuration import Configuration
 from hdx.scraper.geonode.geonodetohdx import GeoNodeToHDX
 from hdx.utilities.downloader import Download
-
-from hdx.facades.simple import facade
 from hdx.utilities.text import remove_string
 
 logger = logging.getLogger(__name__)
 
-lookup = 'hdx-scraper-mimu'
-VERSION_PATTERN = re.compile('[\-\s_]v\d*(-\d*)+')
+lookup = "hdx-scraper-mimu"
+VERSION_PATTERN = re.compile(r"[\-\s_]v\d*(-\d*)+")
 
 
 def process_dataset_name(name):
@@ -29,7 +27,7 @@ def process_dataset_name(name):
 
 
 def create_dataset_showcase(dataset, showcase, **kwargs):
-    logger.info('Dataset date is %s' % dataset['dataset_date'])
+    logger.info(f"Dataset date is {dataset['dataset_date']}")
 
 
 def main():
@@ -37,22 +35,30 @@ def main():
 
     with Download() as downloader:
         configuration = Configuration.read()
-        base_url = configuration['base_url']
-        dataset_tags_mapping = configuration['dataset_tags_mapping']
+        base_url = configuration["base_url"]
+        dataset_tags_mapping = configuration["dataset_tags_mapping"]
         geonodetohdx = GeoNodeToHDX(base_url, downloader)
-        countrydata = {'iso3': 'MMR', 'name': 'Myanmar', 'layers': None}
-        metadata = {'maintainerid': '196196be-6037-4488-8b71-d786adf4c081',
-                    'orgid': 'bde18602-2e92-462a-8e88-a0018a7b13f9'}
+        countrydata = {"iso3": "MMR", "name": "Myanmar", "layers": None}
+        metadata = {
+            "maintainerid": "196196be-6037-4488-8b71-d786adf4c081",
+            "orgid": "bde18602-2e92-462a-8e88-a0018a7b13f9",
+        }
 
-        datasets = geonodetohdx.generate_datasets_and_showcases(metadata, countrydata=countrydata,
-                                                                get_date_from_title=True,
-                                                                process_dataset_name=process_dataset_name,
-                                                                dataset_tags_mapping=dataset_tags_mapping,
-                                                                updated_by_script='HDX Scraper: MIMU GeoNode')
+        datasets = geonodetohdx.generate_datasets_and_showcases(
+            metadata,
+            countrydata=countrydata,
+            get_date_from_title=True,
+            process_dataset_name=process_dataset_name,
+            dataset_tags_mapping=dataset_tags_mapping,
+            updated_by_script="HDX Scraper: MIMU GeoNode",
+        )
         geonodetohdx.delete_other_datasets(datasets, metadata)
 
 
-if __name__ == '__main__':
-    facade(main, user_agent_config_yaml=join(expanduser('~'), '.useragents.yml'), user_agent_lookup=lookup, project_config_yaml=join('config', 'project_configuration.yml'))
-
-
+if __name__ == "__main__":
+    facade(
+        main,
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yml"),
+        user_agent_lookup=lookup,
+        project_config_yaml=join("config", "project_configuration.yml"),
+    )
